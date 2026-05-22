6c0b42fbdcd44a668e9f814d596183fd20df99ea
Ravi Kumar Yadav <ravikumaryaadav1511@gmail.com>
2026-05-22 18:15:31 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ dotenv.config()
 
 const app = express();
 app.use(cors({
-    origin:"https://mock-interviewer-ai-client.onrender.com" || "http://localhost:5173",
-    credentials: true
+  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  credentials: true
 }));
 
 app.use(express.json())
